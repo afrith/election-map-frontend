0a99dcaa2c319700885a8fa7cab4db0dd5f93e63
Adrian Frith <adrianf@da.org.za>
2019-05-25 14:33:33 +0200

    Initial map display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import MainMap from './MainMap'
 
 function App() {
   return (
+    <MainMap />
   );
 }
 
---
+++ src/MainMap.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import ReactMapboxGl, { Source, Layer } from 'react-mapbox-gl'
+
+import { leadingPartyColor } from './util/styles'
+
+const ReactMap = ReactMapboxGl({
+  accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+  hash: true,
+  dragRotate: false,
+  pitchWithRotate: false,
+  touchZoomRotate: false
+});
+
+const defaultZoom = [5]
+const defaultCenter = [25,-28.5]
+
+const source = (
+  <Source
+    id="vdistrict"
+    tileJsonSource={{
+      "type": "vector",
+      "tiles": [`http://localhost:4000/vd_2019/{z}/{x}/{y}/tile.mvt`],
+      "minzoom": 0,
+      "maxzoom": 19
+    }}
+    geoJSONSourceOptions={{ generateId: true }}
+  />)
+  
+const layers = [
+  <Layer
+    id="structure-fill"
+    key="structure-fill"
+    type="fill"
+    sourceId="vdistrict"
+    sourceLayer="vd_2019"
+    paint={{
+      "fill-color": leadingPartyColor('nat'),
+      "fill-opacity": ["case",
+        ["boolean", ["feature-state", "hover"], false],
+        1,
+        0.5
+      ]
+    }}
+  />,
+  <Layer
+    id="structure-line"
+    key="structure-line"
+    type="line"
+    sourceId="vdistrict"
+    sourceLayer="vd_2019"
+    layout={{
+      "line-cap": "butt",
+      "line-join": "bevel"
+    }}
+    paint={{
+      "line-color": "rgb(150,150,150)",
+      "line-width": 0.5,
+      "line-opacity": 1
+    }}
+  />
+]
+
+export default class MainMap extends Component {
+  state = {
+    hoverCode: null
+  }
+
+  render () {
+    return (
+      <div className="map-container">
+        <ReactMap
+          containerStyle={{height: "100%", width: "100%"}}
+          // eslint-disable-next-line react/style-prop-object
+          style="mapbox://styles/mapbox/light-v9"
+          zoom={defaultZoom} center={defaultCenter}
+          onMouseMove={this.onMouseMove}
+        >
+          {source}
+          {layers}
+        </ReactMap>
+      </div>
+    )
+  }
+}
