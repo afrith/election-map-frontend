3c68955789fc2e29bd07a3d54bd1d3e6eaf9917b
Adrian Frith <adrianf@da.org.za>
2019-05-30 17:32:05 +0200

    Fix the colourblind-friendly theme.

--- src/styles/colorBlind.js
+++ src/styles/colorBlind.js
@@ -1,8 +1,8 @@
 import { buildLeadingPartyExpression, buildLeadingPartyLegend } from './utils'
 
 const partyColors = [
-  {abbr: 'ANC', name: 'AFRICAN NATIONAL CONGRESS', colors: ['rgb(199, 233, 192)', 'rgb(161, 217, 155)', 'rgb(116, 196, 118)', 'rgb(65, 171, 93)', 'rgb(35, 139, 69)', 'rgb(0, 90, 50)']},
-  {abbr: 'DA', name: 'DEMOCRATIC ALLIANCE', colors: ['rgb(252,187,161)', 'rgb(252,146,114)', 'rgb(251,106,74)', 'rgb(239,59,44)', 'rgb(203,24,29)', 'rgb(153,0,13)']}
+  {abbr: 'ANC', name: 'AFRICAN NATIONAL CONGRESS', colors: ['rgb(252,187,161)', 'rgb(252,146,114)', 'rgb(251,106,74)', 'rgb(239,59,44)', 'rgb(203,24,29)', 'rgb(153,0,13)']},
+  {abbr: 'DA', name: 'DEMOCRATIC ALLIANCE', colors: ['rgb(198,219,239)', 'rgb(158,202,225)', 'rgb(107,174,214)', 'rgb(66,146,198)', 'rgb(33,113,181)', 'rgb(8,69,148)']}
 ]
 
 const otherColors = ['rgb(217,217,217)', 'rgb(189,189,189)', 'rgb(150,150,150)', 'rgb(115,115,115)', 'rgb(82,82,82)', 'rgb(37,37,37)']
